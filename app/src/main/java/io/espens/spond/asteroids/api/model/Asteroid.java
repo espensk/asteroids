2ff4a970309b2a511d2d6822ac83e8a2d343842e
Espen Skjaeran <espen.skjaeran@telenordigital.com>
2022-06-26 23:44:56 +0200

    javadocing our Asteroid representation

--- app/src/main/java/io/espens/spond/asteroids/api/model/Asteroid.java
+++ app/src/main/java/io/espens/spond/asteroids/api/model/Asteroid.java
@@ -3,5 +3,13 @@ package io.espens.spond.asteroids.api.model;
 import java.math.BigDecimal;
 import java.net.URL;
 
+/**
+ * Documents an Asteroid, a simplified object from the Near Earth Object Web Service
+ * @param id - the objects ID in NASA
+ * @param name - the objects given name
+ * @param diameter - the objects maximum estimated diameter in meters
+ * @param distance - the minimum estimated distance from earth at closest fly-by, in kilometers
+ * @param link - the URL to detailed object description
+ */
 public record Asteroid(String id, String name, BigDecimal diameter, BigDecimal distance, URL link) {
 }
